435384597538ad8461af536ef8227d8386ade2aa
g6g <gaetan.guibaud@gmail.com>
2025-09-18 10:51:20 +0200

    improve paylaod import and  make it generic

--- poc/import.js
+++ poc/import.js
@@ -1,16 +1,45 @@
 
 function extract() {
-    var ifrm = document.createElement("iframe");
-    ifrm.setAttribute("src", "/adresses");
-    ifrm.style.width = "800px";
-    ifrm.style.height = "800px";
-    document.body.appendChild(ifrm);
+    const urlParams = new URLSearchParams(window.location.search);
+    let pathClick = urlParams.get("pathExtract")?? '/adresses';
+    let extract = urlParams.get("extract") ?? "addresslist";
 
-    ifrm.onload = function () {
+    var ifrmExtract = document.createElement("iframe");
+    ifrmExtract.setAttribute("src", pathClick);
+    ifrmExtract.style.width = "800px";
+    ifrmExtract.style.height = "800px";
+    document.body.appendChild(ifrmExtract);
+
+    ifrmExtract.onload = function () {
         setTimeout(function () {
             var contentWindow = ifrm.contentWindow;
-            alert(contentWindow.document.getElementById("addresslist").outerHTML);
-        }, 6000); // Wait for 1 second before executing the code
+            alert(contentWindow.document.getElementById(extract).outerHTML);
+        }, 2000);
     };
 }
+
+function click() {
+
+    const urlParams = new URLSearchParams(window.location.search);
+    let pathClick = urlParams.get("pathClick")?? '/my-account';
+    let clickOnName = urlParams.get("clickon") ?? "contactphone";
+
+    var ifrmCLick = document.createElement("iframe");
+    ifrmCLick.setAttribute("src", pathClick);
+    ifrmCLick.style.width = "800px";
+    ifrmCLick.style.height = "800px";
+    document.body.appendChild(ifrm);
+    console.log(1)
+    ifrmCLick.onload = function () {
+        setTimeout(function () {
+            var contentWindow = ifrm.contentWindow;
+            console.log(2)
+            // récuperer les elements souhaités et faire les actions voulus
+            contentWindow.document.getElementsByName(clickOnName)[0]?.click()
+
+            console.log(iframe.contentWindow.document?.getElementsByName(clickOnName)[0] + 'clicked' )
+        }, 2000);
+    };
+}
+click();
 extract();
